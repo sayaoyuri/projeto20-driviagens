8f964bca5599f0e4c82a7c87f873b1deb16ba738
sayaoyuri <yuriantunes0000@gmail.com>
2023-09-08 14:43:27 -0300

    feat (FLIGHT): implementing origin x destination validation

--- src/services/flightServices.js
+++ src/services/flightServices.js
@@ -1,6 +1,9 @@
+import { errors } from "../errors/errors.js";
 import { flightRepository } from "../repositories/flightRepositories.js";
 
 async function create (origin, destination, date) {
+  if(origin === destination) throw errors.bodyConflict('Origem e Destino');
+
   await flightRepository.create(origin, destination, date);
 };
 
